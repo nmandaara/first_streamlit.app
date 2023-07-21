8a6a7191ce69657dd490e70ce505e3b7f8f43fd7
Mandaara <nmandaar@allegisglobalsolutions.com>
2023-07-21 11:40:44 -0400

    Update streamlit_app.py
    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,38 @@ import streamlit
 import snowflake.connector
 import pandas
 
-streamlit.title("My Parents New Healthy Diner")
+# streamlit.title("My Parents New Healthy Diner")
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+
+# my_data_row = my_cur.fetchone()
+# streamlit.text("Hello from Snowflake: ")
+# streamlit.text(my_data_row)
+streamlit.title("Zena\'s Amazing Athleisure Catalog")
+
+#Connect to Snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake: ")
-streamlit.text(my_data_row)
+#run a snowflake query and put it all in a var called my_catalog
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
+
+#put into dataframe
+df = pandas.DataFrame(my_catalog)
+
+#put first column into a list
+color_list = df[0].values.tolist()
+
+#picklist
+option = streamlit.selectbox("Pick a sweatsuit color or style: ", list(color_list))
+product_caption = "Our warm, comfortable, "+option+" sweatsuit"
+
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style== '" + option + "';")
+df2 = my_cur.fetchone()
+
+streamlit.image(df2[0], width=400, caption = product_caption)
+streamlit.write("Price: ", df2[1])
+streamlit.write("Sizes Available: ", df2[2])
+streamlit.write(df2[3])
