81288cd3422a03ab20bb81c39f00f77df6a7abf1
Nikhil M <36548930+nmandaara@users.noreply.github.com>
2023-07-21 14:01:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,12 @@ streamlit.title('Zena\'s Amazing Athleisure Catalog')
 # connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+
 # run a snowflake query and put it all in a var called my_catalog
-my_cur.execute("select color_or_style from ZENAS_ATHLEISURE_DB.catalog_for_website")
+my_cur.execute("select current_account()")
+streamlit.write(my_cur)
 my_catalog = my_cur.fetchall()
+
 # put the dafta into a dataframe
 df = pandas.DataFrame(my_catalog)
 # temp write the dataframe to the page so I Can see what I am working with
